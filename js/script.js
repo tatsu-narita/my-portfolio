f18860a629260ebc2ad1af6462508b06cca3088e
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-02-25 21:14:28 +0900

    update: js
    javascript変更

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('.main-nav');
+
 btn.addEventListener('click', () => {
     nav.classList.toggle('open-menu');
     if (btn.innerHTML === 'Menu') {
