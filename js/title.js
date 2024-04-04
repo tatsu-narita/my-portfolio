b538a566c94641cb9f308e6e803d84185550536e
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-04-05 02:08:59 +0900

    update: title.js
    titleが現れるアニメーションを追加

--- js/title.js
+++ js/title.js
@@ -1,1 +1,13 @@
-const heading = document.querySelectorAll('page-title');
+const heading = document.querySelector('#page-title');
+
+const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 100px', 0],
+};
+const options = {
+    duration: 2000,
+    easing: 'ease',
+    iteration: Infinity,
+};
+
+heading.animate(keyframes, options);
